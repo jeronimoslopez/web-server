ee2746ea5b5483d7f514cd9216d19630da18fcf2
Jeronimo Lopez <jeronimoslopez@gmail.com>
2016-09-22 09:59:44 -0500

    Refactored middleware

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    logger: function (req, res, next) {
-        var date = new Date().toString();
-        console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware');
 
 app.use(middleware.logger);
 
@@ -20,7 +10,7 @@ app.get('/about', middleware.requireAuthentication , function (req, res) {
     res.send('About Us');
 });
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
     console.log('Express server started on port: ' + PORT);
